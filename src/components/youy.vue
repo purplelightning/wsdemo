606b92ad32c150343a6ec012ae60fe306333fb13
purplelightning <1979506141@qq.com>
2017-10-16 17:02:11 +0800

    Initial commit

---
+++ src/components/youy.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="youy">
+    <h1>{{mm}}</h1>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data (){
+    return  {
+      mm:"这是新创建的用来测试的组件"
+    }
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  color:red
+</style>
