c74b35cc008a1f82aad8c96be1ebc22b9d87ee41
purplelightning <1979506141@qq.com>
2017-10-16 20:17:03 +0800

    vue-router的使用

--- src/main.js
+++ src/main.js
@@ -1,9 +1,37 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
+import goods from './components/goods/goods'
+import ratings from './components/ratings/ratings'
+import seller from './components/seller/seller'
+
+Vue.use(VueRouter);
 
 //eslint允许new Vue而不赋值给对象
 /* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+// new Vue({
+//   el: 'body',
+//   components: { App }
+// });
+
+//使用vue-router要改写js
+let app = Vue.extend(App);
+
+let router = new VueRouter({
+  linkActiveClass:'active'//配置 vue-router带来的v-link-active的别名
 });
+
+router.map({
+  '/goods': {
+    component: goods
+  },
+  '/ratings':{
+    component:ratings
+  },
+  'seller':{
+    component:seller
+  }
+});
+
+router.start(app,'#app');
+router.go('/goods');//默认页面
