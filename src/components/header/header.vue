450a69216164dd5b91b1cef0236c0975f59a35ed
purplelightning <1979506141@qq.com>
2017-10-17 19:54:55 +0800

    star组件建立

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -36,16 +36,8 @@
     <div v-show="detailShow" class="detail">
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
-          {{seller.bulletin}}
-          {{seller.bulletin}}
-          {{seller.bulletin}}
-          {{seller.bulletin}}
-          {{seller.bulletin}}
-          {{seller.bulletin}}
-          {{seller.bulletin}}
-          {{seller.bulletin}}
-          {{seller.bulletin}}
-          {{seller.bulletin}}
+          <h1 class="name">{{seller.name}}</h1>
+
         </div>
       </div>
       <div class="detail-close">
@@ -213,9 +205,15 @@
       background: rgba(7, 17, 27, 0.8)
       .detail-wrapper
         min-height:100%
+        width:100%
         .detail-main
           margin-top:64px
           padding-bottom:64px
+          .name
+            line-height:16px
+            text-align: center
+            font-size:16px
+            font-weight:700
       .detail-close
         position: relative
         width: 32px
