0a75a1e40d5f5bc02ef24f28982a4a74b3d340f0
purplelightning <1979506141@qq.com>
2017-10-23 13:09:47 +0800

    打包过程的实现

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,8 @@
         <a v-link="{path:'/seller'}">商家</a>
       </div>
     </div>
-
-    <router-view :seller="seller"></router-view>
+  <!--keep-alive保留组件的状态 -->
+    <router-view :seller="seller" keep-alive></router-view>
 
   </div>
 </template>
